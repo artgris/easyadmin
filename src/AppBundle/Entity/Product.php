8b15ae12f82ffe1a2da1798fc7953b56a3167fcc
artgris <a.gribet@gmail.com>
2017-05-03 18:03:16 +0200

    tests | documentation

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -50,7 +50,7 @@ class Product
         // otherwise the event listeners won't be called and the file is lost
         if ($image) {
             // if 'updatedAt' is not defined in your entity, use another property
-            $this->updatedAt = new \DateTime('now');
+            $this->updatedAt = new \DateTimeImmutable();
         }
     }
 
