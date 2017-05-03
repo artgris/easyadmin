54425dddc6119738c71d500f4f1606fce2285161
artgris <a.gribet@gmail.com>
2017-05-03 17:46:01 +0200

    tests | documentation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
 	        new JMS\AopBundle\JMSAopBundle(),
 	        new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
 	        new A2lix\TranslationFormBundle\A2lixTranslationFormBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace AppBundle\Controller\Admin;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class UserController extends BaseAdminController
+{
+
+    public function createNewUserEntity()
+    {
+        return $this->get('fos_user.user_manager')->createUser();
+    }
+
+    public function prePersistUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+
+    public function preUpdateUserEntity($user)
+    {
+        $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+//
+//    public function duplicateAction()
+//    {
+//        $id = $this->request->query->get('id');
+//
+//        $entity = clone $this->em->getRepository('AppBundle:User')->find($id);
+//        $entity->setUsername($entity->getUsername()."-1");
+//        $entity->setEmail($entity->getEmail()."-1");
+//        $this->em->persist($entity);
+//        $this->em->flush();
+//        return $this->redirectToRoute('easyadmin', array(
+//            'action' => 'list',
+//            'entity' => $this->request->query->get('entity'),
+//        ));
+//    }
+
+}
